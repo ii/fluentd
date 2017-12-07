bd6f3c1774e71999433c9585ccdac88d7636cd1b
Yuki Ito <yuki@gnnk.net>
2017-12-07 02:48:49 -0600

    Add so_rcvbuf parameter for in_udp

--- lib/fluent/plugin/socket_util.rb
+++ lib/fluent/plugin/socket_util.rb
@@ -32,8 +32,11 @@ module Fluent
     module_function :create_udp_socket
 
     class UdpHandler < Coolio::IO
-      def initialize(io, log, body_size_limit, callback, resolve_hostname = false, remove_newline = true)
+      def initialize(io, log, body_size_limit, callback, resolve_hostname = false, remove_newline = true, so_rcvbuf = nil)
         super(io)
+        if io.is_a?(UDPSocket) && so_rcvbuf
+          io.setsockopt(Socket::SOL_SOCKET, Socket::SO_RCVBUF, so_rcvbuf)
+        end
         @io = io
         @io.do_not_reverse_lookup = !resolve_hostname
         @log = log
